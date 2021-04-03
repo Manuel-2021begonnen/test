d1aa5f41d7e2ac21b3561988736e61e9881e3ff9
Manuel-2021begonnen <81854727+Manuel-2021begonnen@users.noreply.github.com>
2021-04-03 11:40:17 +0200

    Add files via upload
    dit is een script voor web whatsapp wat nog verder wordt uitgebreid.

---
+++ Test Userscript.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Test Userscript
+// @namespace    https://web.whatsapp.com
+// @version      0.1
+// @description  try to take over the world!
+// @author       Manuel Mulder
+// @match        https://web.whatsapp.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert('Hoi ik ben Manuel!');
+})();
